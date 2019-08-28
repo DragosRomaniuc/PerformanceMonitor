90f4801cebfc49f371f0d8200e315f932f80a526
Dragos Adrian ROMANIUC <dromaniuc@bitdefender.com>
2019-08-28 19:31:32 +0300

    .

--- client/server.js
+++ client/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 // app.use(express.static(path.join(__dirname, 'build')));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(__dirname + '/build'));
 app.use(cors());
 app.get('/ping', function (req, res) {
     return res.send('pong');
